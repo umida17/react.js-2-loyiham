4ab85958abedd4ea61e4ec311a85a75157466569
uy <a.normurzaev20@gmail.com>
2026-04-30 22:30:17 +0500

    header bolimi

---
+++ src/Companets/Header/Header.jsx
@@ -0,0 +1,25 @@
+import './Header.css';
+
+import insure from "../Header/Hero/INSURE.svg"; 
+ 
+ function Header() {
+    return (
+        <>
+            <div className="header">
+                  <div className="container">
+                      <div className="header-container">
+                        <img src={insure} alt="" />
+                        <div className="home">
+                            <a href="#">HOW WE WORK</a>
+                            <a href="#">BLOG</a>
+                            <a href="#">ACCOUNT</a>
+                            <a href="#">VIEW PLANS</a>
+                        </div>
+                    </div>
+                  </div>
+            </div>
+        </>
+    )
+ }
+
+ export default Header;
