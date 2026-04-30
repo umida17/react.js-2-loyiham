509afb6b0c07eae50673e0bb2147b27b78a245fc
uy <a.normurzaev20@gmail.com>
2026-05-01 01:36:12 +0500

    footer bowlandi

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
         <Header/>
         
         <Main/>
-       
+       <section/>
+
+      
     </>
   )
 }
---
+++ src/Companets/footer/footer.jsx
@@ -0,0 +1,21 @@
+import "./Footer.css"
+import insure from "../Footer/foto.png/INSURE.png"
+
+function Footer() {
+    return(
+        <>
+        <div className="Footer">
+        <div className="container">
+            <div className="footer-container">
+                <img src={insure} alt="" />
+                <div className="icon">
+                    
+                </div>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default Footer
