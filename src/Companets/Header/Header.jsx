b1b512d56d0ae1e6e10a0dc5ee69cdd4b6bf0fe4
uy <a.normurzaev20@gmail.com>
2026-05-02 21:56:27 +0500

    finish

--- src/Companets/Header/Header.jsx
+++ src/Companets/Header/Header.jsx
@@ -9,6 +9,13 @@ import insure from "../Header/Hero/INSURE.svg";
                   <div className="container">
                       <div className="header-container">
                         <img src={insure} alt="" />
+                        <input type="checkbox" id="menu-toggle" />
+                        <label for="menu-toggle" class="menu-icon">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </label>
+
                         <div className="home">
                             <a href="#">HOW WE WORK</a>
                             <a href="#">BLOG</a>
